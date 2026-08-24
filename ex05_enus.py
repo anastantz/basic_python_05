4d55fb94775503e7627b75179b149412e519555b
ana.drmidnight@gmail.com <ana.drmidnight@gmail.com>
2026-08-24 16:19:23 -0300

    feat: Creating source code - English version

---
+++ ex05_enus.py
@@ -0,0 +1,7 @@
+# Program that reads an integer and displays its predecessor and successor
+
+number = int(input("Enter an integer: "))
+predecessor = number - 1
+successor = number + 1
+
+print(f"Analyzing the number {number}, its predecessor is {predecessor} and its successor is {successor}.")
