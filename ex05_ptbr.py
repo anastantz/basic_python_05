84d23b2b25ba8e270ef9a2378c241d16f558a035
ana.drmidnight@gmail.com <ana.drmidnight@gmail.com>
2026-08-24 16:16:22 -0300

    feat: Creating source code - Portuguese version

---
+++ ex05_ptbr.py
@@ -0,0 +1,7 @@
+# Programa que lê um número inteiro e mostra o seu antecessor e seu sucessor
+
+numero = int(input("Digite um número inteiro: "))
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f"Analisando o número {numero}, seu antecessor é {antecessor} e o seu sucessor é {sucessor}.")
